54976588cdd0484ae088308cbf6b4a0b58d5ea01
tom <tom.simul@gmail.com>
2023-10-09 17:40:36 +0200

    Modif config

--- src/main/java/groupe/camembert/Process/Config.java
+++ src/main/java/groupe/camembert/Process/Config.java
@@ -2,15 +2,7 @@ package groupe.camembert.Process;
 
 public class Config {
 
-    public static String projectSourcePath =  "/home/e20220002252/Documents/M2/M2/913/CodeTest";
+    public static String projectSourcePath =  "/home/tom/Documents/M2/913-RestructurationLogiciels/TP1/CodeTest";
     public static String jrePath = System.getProperty("java.home");
-
-    public static void setProjectSourcePath(String path){
-        projectSourcePath = path;
-    }
-
-    public static void setJrePath(String path){
-        jrePath = path;
-    }
 
 }
