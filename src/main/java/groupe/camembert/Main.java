48c75de138155c8274c5d5603cf2a3641cd1ceb7
tom <tom.simul@gmail.com>
2023-10-09 18:28:55 +0200

    Modif option run

--- src/main/java/groupe/camembert/Main.java
+++ src/main/java/groupe/camembert/Main.java
@@ -1,6 +1,7 @@
 package groupe.camembert;
 
 
+import groupe.camembert.Config.Config;
 import groupe.camembert.UI.CLI;
 import groupe.camembert.UI.GUI;
 
@@ -9,13 +10,18 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         if (args.length == 0){
-            System.out.println("Add -GUI or -CLI to choose your IHM");
+            System.out.println("Add -GUI or -CLI <project path from home> to choose your IHM");
         } else {
             switch (args[0]){
                 case "-GUI":
                     new GUI("Analyzer");
                 case "-CLI":
-                    new CLI().run();
+                    if (args.length != 2){
+                        System.out.println("The option for CLI is: -CLI <project path from home>");
+                    } else {
+                        Config.projectSourcePath = args[1];
+                        new CLI().run();
+                    }
             }
         }
     }
